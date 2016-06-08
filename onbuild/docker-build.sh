b978b852facf76f97b8bc30bd4494cd4b93dd0c2
David Zhao (DEVDIV) <dazhao@microsoft.com>
2016-06-08 14:34:24 -0700

    update

--- onbuild/docker-build.sh
+++ onbuild/docker-build.sh
@@ -2,4 +2,4 @@
 
 pushd /app
 
-docker build -t $(</args/DOCKER_IMAGE_REPOSITORY):$(</args/DOCKER_IMAGE_TAG) -f /args/Dockerfile .
+docker build -t $(</args/DOCKER_IMAGE_REPOSITORY):$(</args/DOCKER_IMAGE_TAG) .
